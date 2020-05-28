704c69b281d555cc2217be8e96cd90b02e59dd37
Mark Dickinson <marksamdickinson@gmail.com>
2020-05-28 14:44:45 -0700

    Add project files.

---
+++ DickinsonBros.Encryption.AES.Abstractions/IAESEncryptionService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DickinsonBros.Encryption.AES.Abstractions
+{
+    public interface IAESEncryptionService<T>
+    {
+        string Decrypt(byte[] encrypted);
+        byte[] Encrypt(string rawString);
+    }
+}
